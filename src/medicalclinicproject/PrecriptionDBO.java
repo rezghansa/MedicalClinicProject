16ab38b1752d16293d83cc00135b29b1b0260f3d
hansa <rezghansa@gmail.com>
2017-03-26 17:10:29 +0530

    presc
    Signed-off-by: hansa <rezghansa@gmail.com>

--- src/medicalclinicproject/PrecriptionDBO.java
+++ src/medicalclinicproject/PrecriptionDBO.java
@@ -5,10 +5,42 @@
  */
 package medicalclinicproject;
 
+import java.time.LocalDate;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author hansa
  */
 public class PrecriptionDBO {
+    
+    private final ObjectProperty<LocalDate> prescribeDate;
+    private final IntegerProperty paientId;
+    private final StringProperty symptoms;
+    private final StringProperty diffrentialD;
+    private final StringProperty labsTxt;
+    private final StringProperty phamacyTxt;
+    private final StringProperty prescriptionTxt;
+    private final DoubleProperty amount;
+    private final ListProperty<Examinations> examination;
+    
+
+    public PrecriptionDBO() {
+        this.prescribeDate = null;
+        this.paientId = null;
+        this.symptoms = null;
+        this.diffrentialD = null;
+        this.labsTxt = null;
+        this.phamacyTxt = null;
+        this.prescriptionTxt = null;
+        this.amount = null;
+        this.examination = null;
+    }
+    
+    
     
 }
